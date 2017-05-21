819045493a13b0ac3dbb5506363caf1cd82d1811
alex-newray <aleksandr.newray@gmail.com>
2017-05-21 23:03:34 +0700

    Create keypressListener.js

---
+++ keypressListener.js
@@ -0,0 +1,29 @@
+export const keypressListener = (state={}, action) => {
+  switch (action.type) {
+    case "KEYPRESSLISTENER_SET":
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export const setKeypressListener = (listener) => {
+  return {
+    type:'KEYPRESSLISTENER_SET',
+    payload:listener
+  }
+}
+
+export const listenKeypressListener = () => {
+  return (dispatch, getState) => {
+    let listener = getState().keypressListener
+    listener.listen()
+  }
+}
+
+export const stopListenKeypressListener = () => {
+  return (dispatch, getState) => {
+    let listener = getState().keypressListener
+    listener.stop_listening()
+  }
+}
